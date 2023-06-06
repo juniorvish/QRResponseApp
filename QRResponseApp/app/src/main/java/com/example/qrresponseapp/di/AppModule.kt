a6fcf45d642e029ab1ad01f719a48e984e7beda4
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-06-06 21:26:50 +0530

    Create QRResponseApp/app/src/main/java/com/example/qrresponseapp/di/AppModule.kt

---
+++ QRResponseApp/app/src/main/java/com/example/qrresponseapp/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.qrresponseapp.di
+
+import com.example.qrresponseapp.api.QRResponseApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "https://qrate.live/api"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQRResponseApi(retrofit: Retrofit): QRResponseApi {
+        return retrofit.create(QRResponseApi::class.java)
+    }
+}

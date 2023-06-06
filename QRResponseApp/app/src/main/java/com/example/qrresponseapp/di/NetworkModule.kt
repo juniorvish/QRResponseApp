e3e136257b118ca4c403d9d9b9d591f4beb259cc
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-06-06 21:27:35 +0530

    Create QRResponseApp/app/src/main/java/com/example/qrresponseapp/di/NetworkModule.kt

---
+++ QRResponseApp/app/src/main/java/com/example/qrresponseapp/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.qrresponseapp.di
+
+import com.example.qrresponseapp.api.QRResponseApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://qrate.live/api"
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQRResponseApi(retrofit: Retrofit): QRResponseApi {
+        return retrofit.create(QRResponseApi::class.java)
+    }
+}
